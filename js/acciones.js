ef905fb342ff322a8bf17c78797e0d570426851d
Sharon <sjuarez18336@plataforma-utslp.net>
2022-07-21 13:07:22 -0500

    Avance dos

---
+++ js/acciones.js
@@ -0,0 +1,13 @@
+/*alert('hi')*/
+function suma(){
+
+	var numero1 = parseFloat(document.getElementById('numero1').value);
+	var numero2 = parseFloat(document.getElementById('numero2').value);
+	var resultado = numero1 + numero2;
+
+	document.getElementById('resultado').value = resultado;
+
+	/*return(resultado);*/	
+
+
+}
